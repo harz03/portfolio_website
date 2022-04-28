eee83fcd4d2aa2fc99aaa9dbfa25a39d347b0ef5
Harshit <harshit79090@gmail.com>
2022-04-28 12:14:50 +0530

    Added Home Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React,{useState} from 'react'
-import {FaBars,FaTimes} from 'react-icons/fa'
+import {FaBars,FaTimes,FaGithub,FaLinkedin} from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../Asset/logo.png'
 const Navbar = () => {
     const [nav,setNav] = useState(false)
@@ -30,8 +32,7 @@ const Navbar = () => {
             <li className='py-6 text-4xl'>Work</li>
             <li className='py-6 text-4xl'>Contact</li>
         </ul>
-        {/* Social icon */}
-        <div className='hidden'></div>
+        
     </div>
   )
 }
