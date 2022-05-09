bc97edb41c0fedfdba70075c50da070e1e678038
Harshit <harshit79090@gmail.com>
2022-05-09 18:16:37 +0530

    Changing color scheme and removing home icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,13 +5,13 @@ const Contact = () => {
     <div name = "contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='text-4xl font-bold inline border-b-4 border-violet-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below email-harshit.3851@gmail.com</p>
                 </div>
                 <input className='p-2' type= "text" placeholder = 'Name' name='name'/>
                 <input className='my-4 p-2 ]' type= "email" placeholder = 'Email' name='email'/>
                 <textarea className= "p-2" name='message' rows='10' placeholder="Message"/>
-                <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-700 px-4 py-3 my-8 mx-auto items-center'>Lets Contact</button>
+                <button className='text-white border-2 hover:bg-violet-600 hover:border-violet-800 px-4 py-3 my-8 mx-auto items-center'>Lets Contact</button>
         </form>
     </div>
   )
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,22 +4,27 @@ import HTML from '../Asset/html.png';
 import CSS from '../Asset/css.png';
 import JavaScript from '../Asset/javascript.png';
 import ReactImg from '../Asset/react.png';
-import Node from '../Asset/node.png';
 import FireBase from '../Asset/firebase.png';
-import AWS from '../Asset/aws.png';
 import GitHub from '../Asset/github.png';
 import Tailwind from '../Asset/tailwind.png';
+import Cpp from '../Asset/c-.png';
 
 const Skills = () => {
   return (
     <div name= "skills" className='w-full h-screen bg-[#0a192f] text-gray-300'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p className = "text-4xl font-bold inline border-b-4 border-pink-600">Experience</p>
+                <p className = "text-4xl font-bold inline border-b-4 border-violet-600">Experience</p>
                 <p className='py-4'>These are the technologies I've worked with</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 text-center p-8'>
+                <div className="shadow-md m-3 shadow-[#040c16] hover:scale-110 duration-500">
+                    <img className='w-20 mx-auto' src={Cpp} alt=""/>
+                    <p className='my-4'>Cpp</p>
+                </div>
+                
+                
                 <div className="shadow-md m-3 shadow-[#040c16] hover:scale-110 duration-500">
                     <img className='w-20 mx-auto' src={HTML} alt=""/>
                     <p className='my-4'>HTML</p>
